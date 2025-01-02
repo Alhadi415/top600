46492479270441eb54cd4d4bf10544ec912eff67
Alhadi415 <mwmlhady4@gmail.com>
2025-01-03 00:13:51 +0300

    Add files via upload

---
+++ lecture14.js
@@ -0,0 +1,260 @@
+const questions =[
+    // Question 261
+    {
+        "question": "The arrow here points to:",
+        "options": [
+            "A. Left atrium",
+            "B. Right pulmonary artery",
+            "C. Posterior pericardial effusion",
+            "D. Left pleural effusion"
+        ],"questionImage": "https://alhadi415.github.io/top600/images/14_61q.JPG" ,
+        "answer": "A. Left atrium",
+        "explanation": "Left atrium"
+    },
+// Question 262
+{
+    "question": "The structure denoted by the arrow is:",
+    "options": [
+        "A. Vegetation",
+        "B. Eustachian valve",
+        "C. Edge of atrial septal defect (ASD)",
+        "D. Tricuspid valve"
+    ], "questionImage": "https://alhadi415.github.io/top600/images/14_62q.JPG",
+    "answer": "B. Eustachian valve",
+    "explanation": "The structure is the Eustachian valve. In the vertical plane the inferior vena cava (IVC) is caudal and gets displayed to the left side of the monitor."
+},
+
+// Question 263
+{
+    "question": "The structure shown by the arrow is:",
+    "options": [
+        "A. Artifact",
+        "B. Catheter in right atrium",
+        "C. Thrombus",
+        "D. Loose suture material"
+    ], "questionImage": "https://alhadi415.github.io/top600/images/14_63q.JPG",
+    "answer": "B. Catheter in right atrium",
+    "explanation": "This structure has a double wall with a central lumen, which is suggestive of a catheter. In addition the structure is linear. Suture material will not have a lumen and thrombus is not uniform in diameter and has no central lucency."
+},
+
+// Question 264
+{
+    "question": "The patient may have all of the following except:",
+    "options": [
+        "A. Atrial septal defect",
+        "B. Wolf-Parkinson-White syndrome",
+        "C. Tricuspid regurgitation",
+        "D. Bicuspid aortic valve"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_64q.JPG",
+    "answer": "D. Bicuspid aortic valve",
+    "explanation": "The patient has Ebstein’s anomaly. Note that the downward displacement of the septal leaflet of the tricuspid valve compared to the mitral leaflet attachment. A displacement of >8 mm/M2 is suggestive of Ebstein’s anomaly. The septal leaflet may be large, sail like, and adherent to the ventricular septum. This is frequently associated with ASD, right sided accessory pathway, and tricuspid regurgitation, but not bicuspid aortic valve."
+},
+
+// Question 265
+{
+    "question": "The mitral valve abnormality seen here is:",
+    "options": [
+        "A. Perforation, prolapse of P1 scallop of posterior leaflet",
+        "B. Abnormal P3 scallop",
+        "C. Prolapsing P2 scallop",
+        "D. Anterior leaflet prolapse"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_65q.JPG",
+    "answer": "A. Perforation, prolapse of P1 scallop of posterior leaflet",
+    "explanation": "In this intercommissural view obtained at about 70°, the area denoted by the arrow is the lateral or P1 scallop of the posterior mitral leaflet. P3 is at the medial commissure. Generally, the A2 scallop, that is, middle scallop of the anterior leaflet, is seen in the middle. However, if the probe is rotated counterclockwise to the left the P2 scallop may be seen in this location."
+},
+
+// Question 266
+{
+    "question": "The structure denoted here is:",
+    "options": [
+        "A. Superior vena cava",
+        "B. Inferior vena cava",
+        "C. Right upper pulmonary vein",
+        "D. Main pulmonary artery"
+    ], "questionImage": "https://alhadi415.github.io/top600/images/14_66q.JPG",
+    "answer": "A. Superior vena cava",
+    "explanation": "This is a long axis image through the superior venacava (SVC) and the right atrium. Also note a pacing lead in the SVC. Advancing the probe further down the esophagus will show the bicaval view. Left atrium is seen closer to the transducer, separated by the atrial septum from the right atrium. Rightward or clockwise rotation will display the right upper pulmonary vein, and leftward or counterclockwise rotation will show the ascending aorta."
+},
+
+// Question 267
+{
+    "question": "The numbers 1, 2, and 3 denote the following cusps of the aortic valve:",
+    "options": [
+        "A. Non, left, right coronary cusps",
+        "B. Left, right, non-coronary cusps",
+        "C. Right, left, non-coronary cusps",
+        "D. Non-coronary, right, left cusps"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_67q.JPG" ,
+    "answer": "A. Non, left, right coronary cusps",
+    "explanation": "Note that the probe is in the esophagus and the anterior is away from the transducer, contrary to the short axis view of the aortic valve by transthoracic echocardiogram."
+},
+
+// Question 268
+{
+    "question": "Structure no. 4 denotes:",
+    "options": [
+        "A. Left atrial appendage",
+        "B. Right atrial appendage",
+        "C. Left upper pulmonary vein",
+        "D. Left lower pulmonary vein"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_68q.JPG",
+    "answer": "A. Left atrial appendage",
+    "explanation": "This structure is the left atrial appendage. Structure no. 5 is the right ventricular outflow tract and structure no. 6 is the right atrium."
+},
+
+// Question 269
+{
+    "question": "The structure shown by the arrow is:",
+    "options": [
+        "A. Calcified native aortic valve",
+        "B. Stented bioprosthetic aortic valve",
+        "C. St. Jude bileaflet mechanical aortic valve",
+        "D. Supravalvular aortic stenosis as part of William’s syndrome"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_69q.JPG",
+    "answer": "A. Calcified native aortic valve",
+    "explanation": "This is a calcified native aortic valve. The native leaflets are seen. There are no struts of a bioprosthetic valve visible. A mechanical valve produces intense shadowing with poor visualization of the disc unless an end-on view is obtained."
+},
+
+// Question 270
+{
+    "question": "The M-mode echocardiogram is suggestive of:",
+    "options": [
+        "A. Normal mitral valve motion",
+        "B. Mitral stenosis",
+        "C. Severe aortic regurgitation",
+        "D. High left atrial pressure"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_70q.JPG" ,
+    "answer": "A. Normal mitral valve motion",
+    "explanation": "This M mode is suggestive of normal mitral valve motion. There is normal mitral valve opening with greater early diastolic opening compared to opening associated with left atrial contraction. Valvular mitral stenosis would cause mitral leaflet"
+},
+
+// Question 271
+{
+    "question": "The image shown here is suggestive of:",
+    "options": [
+        "A. Mitral annuloplasty",
+        "B. Catheter in the coronary artery",
+        "C. Biventricular pacemaker or ICD",
+        "D. An artifact"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_71q.JPG" ,
+    "answer": "C. Biventricular pacemaker or ICD",
+    "explanation": "The arrow here depicts a lead in the coronary sinus and is consistent with a biventricular pacemaker."
+},
+
+// Question 272
+{
+    "question": "The structure denoted by the arrow is:",
+    "options": [
+        "A. Left atrial appendage",
+        "B. Left lower pulmonary vein",
+        "C. Left upper pulmonary vein",
+        "D. Right lower pulmonary vein"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_72q.JPG",
+    "answer": "A. Left atrial appendage",
+    "explanation": "The structure denoted by the arrow is the left atrial appendage. This is separated from the left upper pulmonary vein, which is to the posterior with a ridge popularly known as the “coumadin ridge” because of the potential to be misinterpreted as a thrombus. Because this ridge is echoreflective, sometimes one can see thrombus-like artifacts in the appendage as mirror image artifacts. Though the appendage is clearly visualized here, this view alone is not sufficient to rule out a thrombus. Multiple tomographic views have to be obtained through the appendage in its entirety as the appendage may have multiple lobes."
+},
+
+// Question 273
+{
+    "question": "The patient shown here has:",
+    "options": [
+        "A. Valvular aortic stenosis",
+        "B. Subvalvular aortic stenosis",
+        "C. Endocarditis",
+        "D. Hypertrophic obstructive cardiomyopathy"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_73q.JPG",
+    "answer": "B. Subvalvular aortic stenosis",
+    "explanation": "The structure attached to the septum below the aortic valve is a classic subaortic membrane. Occasionally, vegetations can be seen here due to seeding from the aortic valve. This is a diastolic frame and hence aortic valve opening cannot be evaluated."
+},
+
+// Question 274
+{
+    "question": "The arrow is indicative of:",
+    "options": [
+        "A. Diastolic mitral regurgitation",
+        "B. An artifact",
+        "C. Pulmonary vein D wave picked up by the continuous wave cursor",
+        "D. Mitral annular motion superimposed on the mitral flow"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_74q.JPG",
+    "answer": "A. Diastolic mitral regurgitation",
+    "explanation": "This is diastolic mitral regurgitation (MR), which in this patient is probably due to high left ventricular end diastolic pressure (LVEDP) or coexistent severe AR. Other causes of diastolic MR include prolonged PR interval, prolonged A–V delay, or A–V dissociation. The velocity of this signal is about 1.2 m/s, which is high for tissue velocity. Pulmonary vein D wave would be in the opposite direction, that is, in the direction of the mitral E wave."
+},
+
+// Question 275
+{
+    "question": "This patient is likely to have:",
+    "options": [
+        "A. Systolic heart failure",
+        "B. Fiall mitral valve with good left ventricular function",
+        "C. Isolated severe acute aortic regurgitation",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_75q.JPG",
+    "answer": "A. Systolic heart failure",
+    "explanation": "The profile of MR is indicative of severe LV systolic dysfunction in view of prolonged duration of the MR signal and severely reduced dp/dt in the presence of normal QRS duration. In this example the time taken for the MR velocity to rise from 1 to 3 m/s is 60 ms, which translates into an LV positive dp/dt of 530 mmHg/s (32/0.06). Also note that the diastolic filling period is short and the diastolic MR in this patient is likely from high LVEDP, as the PR interval is not unduly prolonged."
+},
+
+// Question 276
+{
+    "question": "This patient is likely to have:",
+    "options": [
+        "A. Papillary muscle rupture",
+        "B. Mitral valve endocarditis",
+        "C. Fibroelastoma",
+        "D. Libman–Sacks endocarditis"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_76q.JPG",
+    "answer": "B. Mitral valve endocarditis",
+    "explanation": "There is a large mass attached to the P1 scallop of the mitral valve with a soft tissue characteristically less echo dense than the mitral leaflets and a secondary thin mass attached to this. The attachment of this lesion is to the atrial side of the mitral leaflet. This is highly consistent with vegetation. Nonbacterial vegetation of Libman–Sacks endocarditis is a complication of systemic lupus erythematosus and is generally smaller, multiple, and verrucous. Fibroelastomas are more echodense, nodular, generally pedunculated and mobile, usually attached to the ventricular side of the mitral valve."
+},
+
+// Question 277
+{
+    "question": "The need for surgical intervention in this patient is:",
+    "options": [
+        "A. Low",
+        "B. Intermediate",
+        "C. High",
+        "D. This is a nonsurgical condition"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_77q.JPG",
+    "answer": "C. High",
+    "explanation": "The vegetation is very large, measuring about 1.5 × 1 cm, and has a high embolic potential in view of its mobility, large size, and mobile elements attached to its tip. It also has a high potential for lack of bacterial clearance with antibiotics alone because of the size. In addition, this patient has severe mitral regurgitation. In general, the indications for surgery include lack of response to medical therapy, valvular disruption, recurrent embolization, abscess formation, and fungal vegetations. Size greater than 1 cm is a relative indication for surgery because of the potential for complications."
+},
+
+// Question 278
+{
+    "question": "The structure denoted by the arrow is:",
+    "options": [
+        "A. Ascending aorta",
+        "B. Main pulmonary artery",
+        "C. Right atrium",
+        "D. Right ventricular outflow tract"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_78q.JPG",
+    "answer": "A. Ascending aorta",
+    "explanation": "Ascending aorta. Also note long vegetation on the aortic valve on its left ventricular side."
+},
+
+// Question 279
+{
+    "question": "The structure indicated by the arrow is:",
+    "options": [
+        "A. Descending thoracic aorta",
+        "B. Coronary sinus",
+        "C. Inferior vena cava",
+        "D. Circumflex coronary artery"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_79q.JPG",
+    "answer": "B. Coronary sinus",
+    "explanation": "This structure is in the posterior A–V groove, is intrapericardial, and is markedly dilated. Dilatation can occur as a result of either increased flow or increased pressure. Causes include persistent left SVC, right heart failure, coronary fistula, and unroofed coronary sinus. Descending thoracic aorta is extrapericardial. Hence, if there is a pericardial effusion, it would be anterior to the aorta and pleural effusion would be posterior to the aorta. This degree of aneurysm of circumflex artery is unusual. The IVC does not course this area."
+},
+
+// Question 280
+{
+    "question": "The arrow indicates:",
+    "options": [
+        "A. Pleural effusion",
+        "B. Pericardial effusion",
+        "C. Pericardial pad of fat",
+        "D. Artifact"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/14_80q.JPG",
+    "answer": "B. Pericardial effusion",
+    "explanation": "This echolucent space is clearly between two layers of the pericardium. The space is totally echolucent, which indicates fluid rather than fat tissue. Speckled appearance in this area would be indicative of epicardial pad of fat. Pericardial pad of fat would be outside the parietal pericardium."
+}
+]
