bd96dc3d659e0907fa856972ba307b2f2e67dd14
Alhadi415 <mwmlhady4@gmail.com>
2025-01-02 23:40:42 +0300

    Add files via upload

---
+++ lecture13.js
@@ -0,0 +1,260 @@
+const questions =[
+    // Question 241
+    {
+        "question": "This patient is likely to have:",
+        "options": [
+            "A. Severe aortic stenosis (AS)",
+            "B. Severe mitral regurgitation (MR)",
+            "C. Severe pulmonary hypertension",
+            "D. Mild AS"
+        ],"questionImage": "https://alhadi415.github.io/top600/images/13_41q.JPG" ,
+        "answer": "A. Severe aortic stenosis (AS)",
+        "explanation": "This is an AS signal. Please pay attention to the onset of the signal, which occurs sometime after the onset of QRS indicative of signal arising during ejection. Typically a peak gradient of >64 mm hg and a mean gradient of 40 mm hg are indicative of severe AS, although these are flow dependent. Valve area is a better indicator of severity of AS. MR signal starts with QRS. TR signal in severe pulmonary hypertension also starts with QRS."
+    },
+// Question 242
+{
+    "question": "For the patient in question 241 the left ventricular outflow tract (LVOT) diameter was 2 cm and the LVOT velocity by pulse Doppler was 1 m/s. The aortic valve area by the continuity equation would be:",
+    "options": [
+        "A. 0.2 cm²",
+        "B. 0.3 cm²",
+        "C. 0.5 cm²",
+        "D. 0.8 cm²"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_42q.JPG",
+    "answer": "C. 0.5 cm²",
+    "explanation": "By the continuity equation \(A_1 V_1 = A_2 V_2\). Hence the aortic valve area \((A_2 = A_1 V_1 / V_2) = 3.14 \times 1 \times 1 \times 1/6 = 0.5 \, \text{cm}^2\) approximately."
+},
+
+// Question 243
+{
+    "question": "Image of the aortic arch shown here is indicative of:",
+    "options": [
+        "A. Aneurysm of the aortic arch",
+        "B. Aortic dissection",
+        "C. Severe coarctation of the aorta",
+        "D. Stented aortic coarctation"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_43q.JPG",
+    "answer": "D. Stented aortic coarctation",
+    "explanation": "Note the increased density of the aortic wall in the area of coarctation. This represents the stent."
+},
+
+// Question 244
+{
+    "question": "This is the continuous wave signal obtained from the pulmonary valve at the mid- to proximal esophageal location. This patient is likely to have:",
+    "options": [
+        "A. Wide open pulmonary regurgitation (PR)",
+        "B. Mild PR",
+        "C. Severe valvular pulmonary stenosis (PS)",
+        "D. Severe subvalvular PS"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_44q.JPG",
+    "answer": "A. Wide open pulmonary regurgitation (PR)",
+    "explanation": "Note the rapid deceleration of the PR signal with rapid equilibration of late diastolic PA and RV pressures. The increased systolic flow velocity is due to increased flow secondary to wide open PR. Note that from this TEE view, you are looking at the pulmonary valve from above, that is, PA side unlike parasternal short axis view."
+},
+
+// Question 245
+{
+    "question": "This patient has vegetation on:",
+    "options": [
+        "A. Aortic valve",
+        "B. Pulmonary valve",
+        "C. Tricuspid valve",
+        "D. Pacemaker lead"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_45q.JPG",
+    "answer": "B. Pulmonary valve",
+    "explanation": "Pulmonary valve. This is an inflow/outflow view of the right ventricle with aortic valve in the middle, tricuspid valve on the left of the panel and pulmonary valve at the bottom."
+},
+
+// Question 246
+{
+    "question": "The appearance of the left atrial cavity is caused by:",
+    "options": [
+        "A. Stasis of blood",
+        "B. Mitral regurgitation",
+        "C. Polycythemia",
+        "D. Hyperdynamic circulation"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_46q.JPG",
+    "answer": "A. Stasis of blood",
+    "explanation": "This is a spontaneous echo contrast (smoke like echo, frequently called smoke), indicating low velocity of flow, predisposes to thrombus formation and is a marker of embolic risk. Intensity of spontaneous echo contrast is affected by the velocity of blood flow and is exaggerated with higher gain settings and a higher transducer frequency. Also note layered thrombus in the body of the left atrium (at the top of the figure)."
+},
+
+// Question 247
+{
+    "question": "The cause of the patient's mitral valve problem is:",
+    "options": [
+        "A. Rheumatic heart disease",
+        "B. Degenerative valve disease",
+        "C. Fen Phen valvulopathy",
+        "D. Ischemic heart disease"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_47q.JPG",
+    "answer": "A. Rheumatic heart disease",
+    "explanation": "Patient has classic rheumatic mitral stenosis. The anterior leaflet is thin with a hockey-stick appearance in diastole, which occurs due to commissural fusion. In degenerative MS there is severe annular calcification, which extends into the leaflets causing stenosis. In fen-Phen valvulopathy leaflets are thick and fibrosed and may result in both MS and MR. Ischemic involvement of the LV without papillary muscle rupture causes restriction of closure and functional MR and not mitral stenosis."
+},
+
+// Question 248
+{
+    "question": "The arrow in this image points to:",
+    "options": [
+        "A. Right atrium (RA)",
+        "B. Coronary sinus",
+        "C. Left atrium (LA)",
+        "D. Right ventricle (RV)"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_48q.JPG" ,
+    "answer": "A. Right atrium (RA)",
+    "explanation": "Right atrium, to the right atrial septum"
+},
+
+// Question 249
+{
+    "question": "The arrow in this image points to:",
+    "options": [
+        "A. Left ventricular (LV) apical thrombus",
+        "B. RV thrombus",
+        "C. Rib artifact",
+        "D. LA thrombus"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_49q.JPG" ,
+    "answer": "A. Left ventricular (LV) apical thrombus",
+    "explanation": "The position of the septal leaflet of the tricuspid valve confirms the chamber in question to be the left ventricle. There is an apical thrombus. Tricuspid valve goes with right ventricle and is more apically placed than the mitral valve."
+},
+
+// Question 250
+{
+    "question": "This patient is likely to have:",
+    "options": [
+        "A. High RA pressure",
+        "B. Pericardial effusion",
+        "C. Aortic dissection",
+        "D. Dilated azygos vein"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_50q.JPG" ,
+    "answer": "A. High RA pressure",
+    "explanation": "The inferior vena cava as confirmed by its connection to the right atrium is dilated and is about 2.5 cm in diameter. This indicates high RA pressure; especially, if associated with reduced collapse with inspiration. Patients on ventilators may have a dilated inferior vena cava (IVC) without high RA pressure. Young patients might have a dilated IVC with collapse on inspiration, which is normal."
+},
+
+// Question 251
+{
+    "question": "The pulmonary vein flow shown here is indicative of:",
+    "options": [
+        "A. Elevated LA pressure with normal end diastolic pressure (EDP)",
+        "B. Elevated LA pressure with elevated EDP",
+        "C. Abnormal LV relaxation with normal EDP",
+        "D. Elevated LVEDP with normal LA pressure"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_51q.JPG",
+    "answer": "B. Elevated LA pressure with elevated EDP",
+    "explanation": "The rapid D-wave deceleration with time < 170 ms indicates high LA pressure. In addition, the S wave is smaller than the D wave. The atrial regurgitation (AR) wave duration is about 220 ms. The normal duration is about 80–100 ms. This is due to increased duration of atrial systole having to pump against elevated LVEDP. Pulmonary vein AR duration greater than mitral A-wave duration is indicative of high LVEDP."
+},
+
+// Question 252
+{
+    "question": "The mitral flow pattern shown here is suggestive of:",
+    "options": [
+        "A. Normal LA pressure",
+        "B. High LA pressure",
+        "C. Atrial mechanical failure",
+        "D. Abnormal LV relaxation with normal LA pressure"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_52q.JPG",
+    "answer": "B. High LA pressure",
+    "explanation": "This inflow pattern shows high E/A ratio. The deceleration time is also short, suggestive of high LA pressure. In pure atrial mechanical failure the E wave is normal, with diminished mitral A-wave amplitude."
+},
+
+// Question 253
+{
+    "question": "This patient has:",
+    "options": [
+        "A. Mitral atresia",
+        "B. Tricuspid atresia",
+        "C. Transposition of great vessels with atrial baffle",
+        "D. Epstein’s anomaly"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_53q.JPG" ,
+    "answer": "B. Tricuspid atresia",
+    "explanation": "The atretic tricuspid valve is shown. There was no ASD, and outflow from RA was through right atrium to pulmonary artery shunt. Patient also had superior vena caval-right pulmonary artery shunt. Both were patent."
+},
+
+// Question 254
+{
+    "question": "The structure denoted by the arrow is likely to be:",
+    "options": [
+        "A. Artifact",
+        "B. Right atrial thrombus",
+        "C. Myxoma",
+        "D. Fibroelastoma"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_54q.JPG",
+    "answer": "B. Right atrial thrombus",
+    "explanation": "Right atrial thrombus in this patient with tricuspid atresia. Because of atriopulmonary shunt, there is right atrial stasis."
+},
+
+// Question 255
+{
+    "question": "This patient is likely to have:",
+    "options": [
+        "A. Normal pulmonary artery (PA) flow",
+        "B. Pulmonary hypertension approaching systemic pressure",
+        "C. Nonsignificant amount of flow from RV to PA",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_55q.JPG",
+    "answer": "C. Nonsignificant amount of flow from RV to PA",
+    "explanation": "There is tricuspid atresia and pulmonary flow is occurring through cavopulmonary or atriopulmonary shunt in the absence of an ASD. This would need low pulmonary vascular resistance and low PA pressure. This patient has a nonrestrictive ventricular septal defect and hence RV systolic pressure would be the same as LV systolic pressure and to maintain low PA pressure the RV outflow should be minimal or nonexistent. In this patient this was accomplished surgically with banding of the pulmonary artery."
+},
+
+// Question 256
+{
+    "question": "This patient has:",
+    "options": [
+        "A. Normal LA appendage",
+        "B. Clot in the LA appendage",
+        "C. Tumor in the LA appendage",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_56q.JPG",
+    "answer": "B. Clot in the LA appendage",
+    "explanation": "This patient has a clot in the LA appendage. This is best visualized with a transeophageal echocardiogram from an upper esophageal location. The appendage may be multilobed and it is important to examine it in multiple tomographic planes."
+},
+
+// Question 257
+{
+    "question": "This patient has:",
+    "options": [
+        "A. Prominent Eustachian valve",
+        "B. Ostium secundum atrial septal defect (ASD)",
+        "C. Ostium primum ASD",
+        "D. Sinus venosus ASD"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_57q.JPG",
+    "answer": "B. Ostium secundum ASD",
+    "explanation": "Ostium secundum ASD. Primum ASD would be in the lower part of the septum and may involve anterior mitral leaflet and A–V conduction. Sinus venosus ASD is in the upper septum near the superior vena cava and may also be associated with anomalous drainage of the right upper pulmonary vein. There is also a rare type of sinus venosus ASD in the vicinity of the IVC. In unroofed coronary sinus, the shunt is from LA to RA through a posterior defect into the coronary sinus such that flow goes through the coronary sinus into the RA"
+},
+
+// Question 258
+{
+    "question": "What type of flow was recorded from the mid-esophageal position?",
+    "options": [
+        "A. Mitral flow",
+        "B. Pulmonary vein flow",
+        "C. Superior vena cava flow",
+        "D. Flow across ASD"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_58q.JPG",
+    "answer": "D. Flow across ASD",
+    "explanation": "This biphasic flow with a systolic-early diastolic component and LA contraction is typical of ASD flow. Pulmonary vein flow and superior vena cava flow would be triphasic, with distinct systolic and diastolic flows with reversal and atrial contraction. Mitral flow has only diastolic components with early and late diastolic components."
+},
+
+// Question 259
+{
+    "question": "This patient has a secundum ASD with dimensions of the defect 3 cm × 2 cm, time velocity intergral (TVI) of flow across the defect is 39 cm, and the heart rate is 70 beats per minute. The approximate shunt flow across the ASD is:",
+    "options": [
+        "A. 12.8 L/min",
+        "B. 3 L/min",
+        "C. 7 L/min",
+        "D. Cannot be calculated"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_59q.JPG",
+    "answer": "A. 12.8 L/min",
+    "explanation": "The shunt flow per beat can be calculated as the product of the TVI of the shunt flow and the anatomic area of the defect. This would be 39 × 3.14 × 1.5 × 1 cc/beat (183 cc). This multiplied by the heart rate gives the shunt flow per minute."
+},
+
+// Question 260
+{
+    "question": "This patient has:",
+    "options": [
+        "A. Aortic stenosis",
+        "B. Normal opening trileaflet aortic valve",
+        "C. Bicuspid aortic valve that opens well",
+        "D. Unicommissural aortic valve"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/13_60q.JPG",
+    "answer": "B. Normal opening trileaflet aortic valve",
+    "explanation": "Normal opening of the trileaflet aortic valve."
+}
+]
