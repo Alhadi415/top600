6454fb6ca16764e4b79c0d11cf80e7e81294d308
Alhadi415 <mwmlhady4@gmail.com>
2025-01-03 00:56:53 +0300

    Add files via upload

---
+++ lecture15.js
@@ -0,0 +1,260 @@
+const questions =[
+   // Question 281
+    {
+        "question": "This image shows a vegetation on the:",
+        "options": [
+            "A. Aortic valve",
+            "B. P2 scallop of mitral valve",
+            "C. P1 scallop of mitral valve",
+            "D. A2 scallop of mitral valve"
+        ],"questionImage": "https://alhadi415.github.io/top600/images/15_81q.JPG",
+        "answer": "B. P2 scallop of mitral valve",
+        "explanation": "This is a long axis cut through the mitral valve, which courses through the middle of both the anterior and posterior leaflets and hence would show A2 and P2 scallops, respectively. Pushing the probe down will cut through A3 and P3 scallops and pulling the probe up will cut through A1 and P1 scallops."
+    },
+// Question 282
+{
+    "question": "The hemodynamics in this patient potentially could be improved by:",
+    "options": [
+        "A. Increasing the heart rate",
+        "B. Decreasing the heart rate",
+        "C. Increasing the preload",
+        "D. Biventricular pacing"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_82q.JPG",
+    "answer": "D. Biventricular pacing",
+    "explanation": "Note that this patient has a markedly dilated LV and very short diastole despite a heart rate of about 70 min\(^{-1}\), very premature atrial contraction with no passive transmitral flow, with diastolic MR and prolonged systole as indicated by the systolic MR signal. All of these indicate poor systolic performance and AV dysynchrony. Hence the hemodynamics is likely to improve with the therapies listed above. The QRS duration in the monitored ECG is 100 ms. However, 12-lead ECG has to be examined for QRS duration. If QRS duration is prolonged, or mechanical asynchrony is demonstrated by echocardiography, then the patient may also benefit from biventricular pacing."
+},
+
+// Question 283
+{
+    "question": "This patient has:",
+    "options": [
+        "A. Flail P3 scallop of the posterior mitral leaflet",
+        "B. Flail A2 scallop of the anterior mitral leaflet",
+        "C. Flail anterior leaflet",
+        "D. Large mitral valve vegetation"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_83q.JPG",
+    "answer": "A. Flail P3 scallop of the posterior mitral leaflet",
+    "explanation": "This patient has a flail P3 scallop of the posterior mitral leaflet. In this near intercommissural view, with slight rightward rotation, the scallops from right to left include A1, A2, and P3. At about a 70–80 degree angle the scallops seen would be P1, A2, and P3. At around 120–130 degrees the scallops seen would be A2 and P2."
+},
+
+// Question 284
+{
+    "question": "The pulse wave Doppler in the right upper pulmonary vein is indicative of:",
+    "options": [
+        "A. Abnormal left ventricular (LV) relaxation",
+        "B. High left atrial (LA) pressure",
+        "C. Mitral stenosis",
+        "D. Severe mitral regurgitation (MR)"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_84q.JPG",
+    "answer": "D. Severe mitral regurgitation (MR)",
+    "explanation": "This is severe MR. Note the holosystolic flow reversal in the pulmonary vein."
+},
+
+// Question 285
+{
+    "question": "This apical four-chamber view shows:",
+    "options": [
+        "A. A pacemaker lead in the right ventricle (RV)",
+        "B. A pacemaker lead in the coronary sinus",
+        "C. Epicardial RV lead",
+        "D. Artifact in the RV"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_85q.JPG",
+    "answer": "A. A pacemaker lead in the right ventricle (RV)",
+    "explanation": "This is an RV endocardial lead. The coronary sinus (CS) is not visualized here. The CS lead tends to be thinner. CS can be imaged with a posterior tilt from this plane. The ICD leads are much thicker than the pacer leads."
+},
+
+// Question 286
+{
+    "question": "The mitral valve opening pattern in this patient is suggestive of:",
+    "options": [
+        "A. Mitral stenosis",
+        "B. High left ventricular end diastolic pressure (LVEDP)",
+        "C. Atrial fibrillation",
+        "D. Normal pattern"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_86q.JPG" ,
+    "answer": "D. Normal pattern",
+    "explanation": "This is an M-mode through the mitral valve showing a normal pattern with E(1) and A(2) waves on the image of normal amplitude and movement of the posterior leaflet, which is a mirror image in the opposite direction. In atrial fibrillation the A wave disappears. High LVEDP is classically characterized by a 'B' hump, which is a positive deflection on the downslope of the A wave. Features of mitral stenosis include mitral leaflet thickening, reduced opening, flatter, EF slope, and paradoxical anterior motion of the posterior leaflet during diastole due to leaflet fusion."
+},
+
+// Question 287
+{
+    "question": "The part of the anatomy and measurement indicated by the line is:",
+    "options": [
+        "A. The sino-tubular junction",
+        "B. Sinus diameter",
+        "C. Sinus height",
+        "D. Aortic annular diameter"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_87q.JPG",
+    "answer": "A. The sino-tubular junction",
+    "explanation": "This is the sino-tubular junction (STJ), which is the junction between the sinus and the tubular portions of ascending aorta. This diameter is usually less than the annulus diameter. The sinus height is the distance between the annulus and the ST junction and is increased in conditions that cause aneurysmal dilatation of the sinus portion of the aorta. Excessive dilatation of the STJ may cause restriction in the closure of the aortic valve and may result in aortic regurgitation in the absence of leaflet pathology and in the presence of normal annulus size. This can be corrected by restoration of the aortic root anatomy with root replacement."
+},
+
+// Question 288
+{
+    "question": "The blood supply to the ventricular septum shown here is:",
+    "options": [
+        "A. Left anterior descending (LAD)",
+        "B. Posterior descending artery",
+        "C. Both",
+        "D. Neither"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_88q.JPG",
+    "answer": "A. Left anterior descending (LAD)",
+    "explanation": "The entire ventricular septum seen here is anterior, supplied by the LAD artery. The first septal perforator of the LAD artery supplies the very proximal septum."
+},
+
+// Question 289
+{
+    "question": "The structure indicated by the arrow in the ascending aorta is likely to be:",
+    "options": [
+        "A. Vegetative aortitis",
+        "B. Flap of aortic dissection",
+        "C. Intraaortic atherosclerotic debris",
+        "D. Supravalvular aortic stenosis"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_89q.JPG" ,
+    "answer": "A. Vegetative aortitis",
+    "explanation": "This thin filamentous mass in association with aortic valve vegetation was vegetation on the aortic wall seeding the right side of the aortic wall as a jet lesion. There is no false lumen or intramural hematoma to support the diagnosis of a flap. The remainder of the aorta is normal without any atherosclerotic changes; however, a small atheromatous mass is still a possibility."
+},
+
+// Question 290
+{
+    "question": "The structure indicated by the arrow is likely to be:",
+    "options": [
+        "A. Aortic dissection",
+        "B. Aortic transaction",
+        "C. Right coronary artery",
+        "D. Left coronary artery"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_90q.JPG" ,
+    "answer": "C. Right coronary artery",
+    "explanation": "This is anterior and the artery is coming out of the right sinus of valsalva on this TEE. Dissection will be characterized by a thin mobile flap and a false lumen. In transection, there would be a thicker, localized flap protruding into the aortic lumen associated with disruption of media and adventitia."
+},
+
+// Question 291
+{
+    "question": "The arrow in this short axis view transthoracic echocardiogram (TTE) image at the level of the ascending aorta is:",
+    "options": [
+        "A. Artifact",
+        "B. Tissue plane and aorta and RV outflow tract",
+        "C. Aortic dissection",
+        "D. Right coronary artery"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_91q.JPG" ,
+    "answer": "D. Right coronary artery",
+    "explanation": "Note the tubular nature of the structure and its continuity with the aortic lumen. Right coronary artery arises anteriorly from this location and the origin of the left coronary artery would be in the 4 o'clock position (not shown here). Here, RCA is originating anomalously from the junction of right and non-coronary sinuses."
+},
+
+// Question 292
+{
+    "question": "The structure shown by the arrow is:",
+    "options": [
+        "A. Coronary sinus",
+        "B. Atrial septal defect (ASD)",
+        "C. Superior vena cava",
+        "D. Inferior vena cava"
+    ],"explanationImage": "https://alhadi415.github.io/top600/images/15_92.JPG",
+    "answer": "A. Coronary sinus",
+    "explanation": "This low esophageal view at the gastro-esophageal junction with the transverse plane in the A-V groove posteriorly demonstrates the coronary sinus draining into the right atrium. A long axis cut through the vena cavae is generally seen in the vertical bicaval view in the 80-120 degree angle."
+},
+
+// Question 293
+{
+    "question": "The valve indicated by the arrow is:",
+    "options": [
+        "A. Pulmonary valve",
+        "B. Aortic valve",
+        "C. Tricuspid valve",
+        "D. Mirror image artifact of the aortic valve"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_93q.JPG",
+    "answer": "A. Pulmonary valve",
+    "explanation": "Note that this is anterior and connects to the PA. Also seen is part of the aortic valve posterior to this structure."
+},
+
+// Question 294
+{
+    "question": "This view is obtained from the upper esophagus. The structure indicated by the arrow is:",
+    "options": [
+        "A. Aortic valve",
+        "B. Pulmonary valve",
+        "C. Tricuspid valve",
+        "D. Artifact"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_94q.JPG" ,
+    "answer": "B. Pulmonary valve",
+    "explanation": "The structure indicated by the arrow is the pulmonary valve. The structure closer to the transducer is the aortic arch in transverse plane. Part of the main PA is seen closer to the transducer. This is a good TEE view for Doppler interrogation of pulmonary valve or main pulmonary artery."
+},
+
+// Question 295
+{
+    "question": "The pulmonary regurgitation signal shown here is indicative of (assuming right atrial pressure of 15 mmHg):",
+    "options": [
+        "A. Normal pulmonary artery (PA) pressure",
+        "B. Mild pulmonary hypertension",
+        "C. Moderate pulmonary hypertension",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_95q.JPG",
+    "answer": "C. Moderate pulmonary hypertension",
+    "explanation": "The end diastolic velocity is \(2.1 \, \text{m/s}\), which translates into an end diastolic gradient of \(17 \, \text{mmHg}\) between the PA and the RV. Assuming the RVEDP to be the same as the mean RA pressure of \(15 \, \text{mmHg}\), the computed PA end diastolic pressure would be \(32 \, \text{mmHg}\). This is consistent with moderate to severe pulmonary hypertension. Also note that the PR signal is rapidly decelerating, indicating either severe PR or rapidly increasing RVEDP."
+},
+
+// Question 296
+{
+    "question": "This subcostal view shows part of the liver. This patient has a history of episodes of flushing and diarrhea. The likely diagnosis is:",
+    "options": [
+        "A. Amebic liver abscess",
+        "B. Right atrial myxoma",
+        "C. Carcinoid syndrome",
+        "D. Renal cell carcinoma"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_96q.JPG",
+    "answer": "C. Carcinoid syndrome",
+    "explanation": "The image shows abnormal liver with a multiple echogenic and echoluscent area consistent with metastatic tumor. This combined with the clinical presentation is indicative of carcinoid syndrome. Valves most commonly affected include the tricuspid and pulmonary valves. Renal cell carcinoma may extend to the heart through the inferior vena cava. Right atrial myxoma usually is attached to the atrial septum or the atrial free wall."
+},
+
+// Question 297
+{
+    "question": "This 86-year-old patient has intractable heart failure and chronic atrial fibrillation. The finding on the still image is suggestive of:",
+    "options": [
+        "A. Left atrial thrombus",
+        "B. Lipomatous atrial septum",
+        "C. ASD closure device",
+        "D. Side lobe artifact"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_97q.JPG",
+    "answer": "A. Left atrial thrombus",
+    "explanation": "This is a left atrial thrombus. A large mass is visualized attached to the atrial septum in the fossa ovalis area. In the given clinical context this is likely to be a thrombus. Differential diagnosis includes left atrial myxoma. Lipomatous atrial septum spares the fossa ovalis but causes thickening of the rest of the septum due to fat deposition. An ASD closure device like the Amplatzer device has a characteristic internal architecture made up of mesh and wires. Note severely dilated left atrium. This patient was in chronic atrial fibrillation predisposing him to left atrial thrombus formation."
+},
+
+// Question 298
+{
+    "question": "In question 297 the left ventricular size and ejection fraction were normal. The patient is likely to have:",
+    "options": [
+        "A. Restrictive cardiomyopathy",
+        "B. Congestive cardiomyopathy",
+        "C. Hypertrophic cardiomyopathy",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_98q.JPG",
+    "answer": "A. Restrictive cardiomyopathy",
+    "explanation": "Severe biatrial enlargement with a normal sized ventricle associated with high filling pressures is diagnostic of restrictive cardiomyopathy. This patient has aneurysmal biatrial enlargement. This patient also had low-voltage electrocardiographic complexes, which is suggestive of cardiac amyloidosis."
+},
+
+// Question 299
+{
+    "question": "The short axis image of this patient shows:",
+    "options": [
+        "A. Posterior pericardial effusion",
+        "B. Massive mitral annular calcification",
+        "C. Calcified aortic valves",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_99q.JPG",
+    "answer": "B. Massive mitral annular calcification",
+    "explanation": "The posterior annulus is massively calcified. The echolucent area posterior to this is due to shadowing because of lack of penetration through this massively calcified structure."
+},
+
+// Question 300
+{
+    "question": "The appearance of the interatrial septum is indicative of:",
+    "options": [
+        "A. Left atrial myxoma",
+        "B. Aneurysmal atrial septum",
+        "C. ASD",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/15_300q.JPG",
+    "answer": "B. Aneurysmal atrial septum",
+    "explanation": "The fossa ovalis is bowing towards the right atrium. This back and forth movement of the fossa is better visualized during dynamic imaging. This is associated with patent foramen ovale and increased risk of stroke and possibly migraine. The image does not show any mass or atrial septal defect, though a tangential cut through the aneurysmal septum may mimic a mass in certain views during dynamic imaging."
+}
+]
