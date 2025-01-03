8e2ba746c3498b10506720aa44df17f99118b9ab
Alhadi415 <mwmlhady4@gmail.com>
2025-01-03 16:34:46 +0300

    Add files via upload

---
+++ lecture17.js
@@ -0,0 +1,261 @@
+const questions =[
+// Question 321
+{
+    "question": "The cause of dyspnea in this patient is likely to be due to:",
+    "options": [
+        "A. Left heart failure",
+        "B. Primary pulmonary hypertension",
+        "C. Chronic obstructive pulmonary disorder",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_1q.JPG",
+    "answer": "A. Left heart failure",
+    "explanation": "The mitral inflow shown here is indicative of high left atrial (LA) pressure. Although the patient is in atrial fibrillation with only E-wave, the E-wave deceleration is very rapid, with a deceleration time of 100 ms."
+},
+
+// Question 322
+{
+    "question": "This is an end systolic frame in a patient with shortness of breath. The most likely diagnosis is:",
+    "options": [
+        "A. Ebstein’s anomaly",
+        "B. Hypertrophic cardiomyopathy",
+        "C. Atrial septal defect",
+        "D. Dilated cardiomyopathy"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_2q.JPG",
+    "answer": "D. Dilated cardiomyopathy",
+    "explanation": "There is a four-chamber dilatation. There is no increase in wall thickness to suggest HOCM. In atrial septal defect, both the right ventricle and the right atrium will be dilated due to volume overload with normal LA and LV size. The tricuspid valve position is normal and hence does not support the diagnosis of Ebstein’s anomaly."
+},
+
+// Question 323
+{
+    "question": "The most likely mechanism of mitral regurgitation (MR) in this patient is:",
+    "options": [
+        "A. P2 tethering",
+        "B. P2 prolapse",
+        "C. Bileaflet mitral valve prolapse",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_3q.JPG",
+    "answer": "A. P2 tethering",
+    "explanation": "This is an apical long-axis view, showing A2 and P2 scallops of the mitral valve. The MR jet is directed posterolaterally toward P2, consistent with P2 tethering. A similar jet direction can also occur in A2 prolapse, but both leaflets coapt distal to the plane of mitral annulus. Bileaflet prolapse of equal magnitude will result in a central jet."
+},
+
+// Question 324
+{
+    "question": "This 19-year-old patient was stabbed in the precordial area. Examination revealed a loud systolic murmur. The most likely cause of this murmur is:",
+    "options": [
+        "A. Penetrating injury to the interventricular septum",
+        "B. Mitral valve prolapse",
+        "C. Hypertrophic obstructive cardiomyopathy (HOCM)",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_4q.JPG" ,
+    "answer": "A. Penetrating injury to the interventricular septum",
+    "explanation": "A defect is seen in the ventricular septum. This patient had a penetrating injury to the septum. The image does not support the presence of mitral valve prolapse or hypertrophic septum."
+},
+
+// Question 325
+{
+    "question": "This transesophageal echocardiogram (TEE) image is obtained from the upper esophagus, and the aortic arch is shown on the top. The arrow points to:",
+    "options": [
+        "A. Pulmonary valve",
+        "B. Aortic valve",
+        "C. Mitral valve",
+        "D. Tricuspid valve"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_5q.JPG",
+    "answer": "A. Pulmonary valve",
+    "explanation": "Pulmonary valve and the main pulmonary artery are seen. This is a good view to examine these structures and also to obtain spectral Doppler signals from the pulmonary valve and pulmonary artery. Main pulmonary artery is closer to the transducer."
+},
+
+// Question 326
+{
+    "question": "The structure indicated by the arrow is:",
+    "options": [
+        "A. Right coronary artery (RCA)",
+        "B. Left coronary artery (LCA)",
+        "C. Entry tear into dissection",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_6q.JPG",
+    "answer": "A. Right coronary artery (RCA)",
+    "explanation": "This is the classical location of the RCA. The LCA is not seen in the aortic long-axis view. Because of its left lateral location, it is seen in the short-axis view. There is no aortic dissection and entry tear into dissection would be a hole in the endothelium and is not tubular in shape."
+},
+
+// Question 327
+{
+    "question": "This is a suprasternal image of the aortic arch, suggestive of:",
+    "options": [
+        "A. Coarctation of the aorta",
+        "B. Severe aortic regurgitation (AR)",
+        "C. Patent ductus arteriosus (PDA)",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_7q.JPG",
+    "answer": "A. Coarctation of the aorta",
+    "explanation": "Narrowing and turbulence at the junction of the arch and descending aorta are clearly seen, indicative of coarctation. In severe AR, flow reversal is holodiastolic. This is a systolic frame (see marker on the ECG). There is no communication between the aorta and the pulmonary artery, suggestive of PDA."
+},
+
+// Question 328
+{
+    "question": "In the accompanying image the structure indicated by the arrow is:",
+    "options": [
+        "A. Right pulmonary artery (RPA)",
+        "B. Left atrium",
+        "C. Aortic arch",
+        "D. Right upper pulmonary vein"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_8q.JPG",
+    "answer": "A. Right pulmonary artery (RPA)",
+    "explanation": "This is a TEE image from the proximal esophageal location, a high basal view above the level of the left atrium. Also note the linear shadows in the RPA, which are commonly seen and represent mirror image artifacts. A short-axis image of the RPA or color flow image at a low scale would confirm this. The large circular structure is the ascending aorta and number 1 is the SVC in cross-section. Also note the reverberations from a catheter in the SVC."
+},
+
+// Question 329
+{
+    "question": "The structure denoted by the arrow is:",
+    "options": [
+        "A. An artifact",
+        "B. Pulmonary valve",
+        "C. Aortic valve",
+        "D. Subpulmonic stenosis"
+    ], "questionImage": "https://alhadi415.github.io/top600/images/17_9q.JPG" ,
+    "answer": "B. Pulmonary valve",
+    "explanation": "This is a TEE image from mid to low esophageal location showing the long axis of the pulmonary valve and the proximal pulmonary artery. The RVOT is clearly seen as well."
+},
+
+// Question 330
+{
+    "question": "What is the abnormality in the accompanying image?",
+    "options": [
+        "A. Congenital muscular ventricular septal defect (VSD)",
+        "B. Postinfarction posterior VSD",
+        "C. Artifact of the normal posterior thinning at the valve plane",
+        "D. Postmyectomy of HOCM"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_30q.JPG",
+    "answer": "B. Postinfarction posterior VSD",
+    "explanation": "There is marked thinning of the midinferior wall and inferior septum without any scarring. There is also a defect establishing communication between the LV and the RV and the color flow confirms this. This is classical postinfarct posterior VSD, and a short-axis view of the LV at all levels would help to delineate the pathology. Such thinning does not occur in congenital VSD. The location of iatrogenic infarct or myectomy in HOCM is in the proximal anterior septum."
+},
+
+// Question 331
+{
+    "question": "The abnormal finding in this image is:",
+    "options": [
+        "A. Bicuspid aortic valve",
+        "B. Aortic dissection flap",
+        "C. Aortic aneurysm",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_31q.JPG",
+    "answer": "A. Bicuspid aortic valve",
+    "explanation": "This is a classic bicuspid aortic valve. Although a dissection flap in a patient with circumferential dissection with a central true lumen may mimic this, both leaflets and commissures are clearly seen here. This image is at the level of the aortic annulus and the rest of the ascending aorta is not shown to comment about nondissecting aortic aneurysm."
+},
+
+// Question 332
+{
+    "question": "Mitral regurgitation (MR) signal shown here is suggestive of:",
+    "options": [
+        "A. Some diastolic MR in addition to systolic MR",
+        "B. Markedly depressed left ventricular (LV) dp/dt",
+        "C. Both",
+        "D. Neither"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_32q.JPG",
+    "answer": "C. Both",
+    "explanation": "The initial portion of the signal at low velocity represents diastolic MR, which occurs during left atrial relaxation and may be due to a long PR interval or high left ventricular end diastolic pressure (LVEDP). The rate of velocity rise of the MR signal is very slow. The normal time taken for the MR velocity to increase from 1 to 3 m/s is 10–20 ms, representing an LV dp/dt of 1600–3200 mmHg/s. In this example, this interval is 160 ms, giving an average rate of pressure rise during early systole (loosely called LV dp/dt and correlates with this) is 200 mmHg/s. Also note the very prolonged QRS duration. The main determinants of LV dp/dt include LV contractility, heart rate, preload, and LV systolic synchrony, which would be abnormal in left bundle branch block."
+},
+
+// Question 333
+{
+    "question": "Mitral flow profile shown here is suggestive of:",
+    "options": [
+        "A. Normal LV diastolic function",
+        "B. Abnormal relaxation",
+        "C. Pseudonormal pattern",
+        "D. Restrictive pattern"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_33q.JPG",
+    "answer": "B. Abnormal relaxation",
+    "explanation": "Suggested by an E/A ratio of <1. Although the E-wave deceleration time is <250 ms, generally the isovolumic relaxation time (not shown here) tends to be greater than 100 ms. The E/A ratio can be lower in the elderly because of age-related LV relaxation failure, patients with low filling pressures, more rapid heart rates, and those with prolongation of the PR interval. In a pseudonormal pattern, the mitral inflow looks normal, but there is some other evidence of LV relaxation abnormality, such as reduced Em velocity, reduced mitral flow propagation, or increased duration of atrial systole as judged by pulmonary vein flow (AR wave reversal duration). Restrictive pattern is characterized by an E/A ratio of >2, E-wave deceleration of <150 ms, and isovolumic relaxation time duration of <70 ms."
+},
+
+// Question 334
+{
+    "question": "This image shows:",
+    "options": [
+        "A. Normal flow in the left ventricular outflow tract (LVOT)",
+        "B. Subvalvular aortic stenosis (AS)",
+        "C. Aortic regurgitation",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_34q.JPG",
+    "answer": "C. Aortic regurgitation",
+    "explanation": "Note the diastolic frame showing an AR jet. The AR is severe as the width of the jet is >60% of the left ventricular outflow tract and has a large vena contracta and PISA."
+},
+
+// Question 335
+{
+    "question": "This continuous wave Doppler signal is suggestive of:",
+    "options": [
+        "A. AS and AR",
+        "B. Mitral stenosis (MS) and MR",
+        "C. VSD flow",
+        "D. Aortic flow in a patient with coarctation"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_35q.JPG" ,
+    "answer": "A. AS and AR",
+    "explanation": "Unlike the MR signal, the AS signal occupies only the ejection period and is absent during the isovolumic contraction time. Hence, the signal starts a few milliseconds after the QRS. The diastolic velocity is too high for MS. This order of gradient and LA pressure would be incompatible with life. VSD flow is predominantly systolic with a presystolic component caused by atrial contraction. The gradient across a coarctation is systolic, the duration increases with greater degrees of stenosis, and there may be a diastolic gradient. However, both the systolic and diastolic components will be in the same direction."
+},
+
+// Question 336
+{
+    "question": "This continuous wave signal obtained from the midtransesophageal location is indicative of:",
+    "options": [
+        "A. AS and AR",
+        "B. MS and MR",
+        "C. VSD flow",
+        "D. None of the above"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_36q.JPG" ,
+    "answer": "B. MS and MR",
+    "explanation": "Please see explanation to question 335. Note that in TEE, from midesophageal location, MR jet is directed toward the transducer."
+},
+
+// Question 337
+{
+    "question": "This is a TEE image from the midesophagus of a late diastolic frame of the aortic valve. This patient is most likely to have:",
+    "options": [
+        "A. Severe aortic regurgitation",
+        "B. Severe aortic stenosis",
+        "C. HOCM",
+        "D. Ascending aortic dissection"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_37q.JPG",
+    "answer": "A. Severe aortic regurgitation",
+    "explanation": "The aortic leaflets are thickened with rolled-up edges and a central coaptation defect in end diastole. This anatomy would be associated with wide-open aortic regurgitation, as the regurgitant orifice is visible by anatomic imaging. Aortic stenosis cannot be diagnosed by a diastolic frame. HOCM typically causes midsystolic closure of the aortic valve, best visualized by M mode. Aortic dissection may cause AR by one of several mechanisms: dilatation of sino–tubular junction, leaflet tethering, extension of hematoma into the aortic leaflet causing it to prolapse, or as a result of the primary problem such as bicuspid aortic valve or annulo-aortic ectasia."
+},
+
+// Question 338
+{
+    "question": "This patient is most likely to have:",
+    "options": [
+        "A. Acute severe MR",
+        "B. Chronic severe MR",
+        "C. Severe MS and mild MR",
+        "D. None of the above"
+    ], "questionImage": "https://alhadi415.github.io/top600/images/17_38q.JPG",
+    "answer": "B. Chronic severe MR",
+    "explanation": "The MR signal density is more than 60% of the mitral inflow signal. This correlates with a volume of regurgitation. In addition, the mitral inflow velocity is increased without a slow deceleration. A slow deceleration would indicate significant mitral stenosis. Despite severe MR, the profile of the MR signal is quite rounded without the rapid deceleration that would typically be seen in acute severe MR, because of the large left atrial V-wave, the so-called “V-wave cutoff sign.” Note that the heart rate is 151 beats per minute. At rapid heart rates, MR may be grossly underestimated by color Doppler due to limited temporal resolution and continuous wave Doppler is very helpful."
+},
+
+// Question 339
+{
+    "question": "This patient had Staphylococcus aureus endocarditis of the aortic valve. The most likely cause is:",
+    "options": [
+        "A. Central venous catheter-associated infection",
+        "B. Dental work",
+        "C. Immunosuppressed state",
+        "D. Intravenous drug use"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_39q.JPG",
+    "answer": "A. Central venous catheter-associated infection",
+    "explanation": "This bicaval transesophageal view shows a large mass in the superior vena cava, which is typically associated with a central catheter-associated thrombus or vegetation. This most likely is the cause of his sepsis and endocarditis. In addition, there is a possible defect at the superior portion of the fossa ovalis, suggesting a patent foramen ovale (PFO). This patient had a large PFO by color and contrast echocardiography, allowing paradoxical embolization of the bacterial mass to cause left-sided endocarditis, escaping the protective filtration mechanism offered by the lung."
+},
+
+// Question 340
+{
+    "question": "The image of the aortic valve is suggestive of:",
+    "options": [
+        "A. Aortic valve vegetation",
+        "B. Node of Arantius",
+        "C. Lambl's excrescences",
+        "D. Ascending aortic dissection causing prolapse of the noncoronary cusp"
+    ],"questionImage": "https://alhadi415.github.io/top600/images/17_40q.JPG",
+    "answer": "A. Aortic valve vegetation",
+    "explanation": "The mass on the aortic valve is suggestive of mass on the left ventricular side of the aortic valve. This is suggestive of vegetation. Also, there is prolapse of the noncoronary cusp, causing significant AR, and this is due to leaflet destruction with endocarditis. The other mechanism for prolapse could be a bicuspid aortic valve with prolapse of the larger cusp. Node of Arantius, as the name suggests, is a nodular thickening of the central portion of the leaflet edge and is best visualized from the short-axis view of the valve. Lambl’s excrescences are thin filamentous structures attached to the leaflet margin. There is no evidence of aortic dissection or intramural hematoma in this patient."
+}
+]
